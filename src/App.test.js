90710086d0ad2cc61b12dc1fbb5520fee72aa489
Robert Guinn <80178634+RGuinn1207@users.noreply.github.com>
2021-11-14 15:57:29 -0700

    w30assg

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+// });
+
+const Deposit = require("./App");
+ describe("My banking-app", () => {
+   test("works", () => {
+    expect("Deposit.Deposit"()).toEqual("Deposit");
+  });
+ });
